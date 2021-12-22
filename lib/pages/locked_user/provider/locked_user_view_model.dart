fc36bc06c669f2a7d4d2babfcdde39c6023263d4
imi <89938440+2i2i@users.noreply.github.com>
2021-12-21 18:02:06 -0700

    ...

--- lib/pages/locked_user/provider/locked_user_view_model.dart
+++ lib/pages/locked_user/provider/locked_user_view_model.dart
@@ -2,7 +2,6 @@ import 'package:app_2i2i/models/meeting.dart';
 import 'package:app_2i2i/models/user.dart';
 import 'package:app_2i2i/services/logging.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class LockedUserViewModel with ChangeNotifier {
   LockedUserViewModel({required this.user, required this.meeting});
@@ -19,38 +18,5 @@ class LockedUserViewModel with ChangeNotifier {
     final x = meeting.B == user.id;
     log('LockedUserViewModel - amA - x=$x');
     return x;
-  }
-
-  bool _isMute = false;
-  bool _isDisableVideo = false;
-
-
-  bool get isMute => _isMute;
-
-  bool get isDisableVideo => _isDisableVideo;
-
-  set isMute(bool value) {
-    _isMute = value;
-    notifyListeners();
-  }
-
-
-  set isDisableVideo(bool value) {
-    _isDisableVideo = value;
-    notifyListeners();
-  }
-
-
-
-  Future<void> muteCall() async {
-
-
-    // if (mediaStream != null) {
-    //   mediaStream.getAudioTracks()[0]?.enabled = false;
-    //   mediaStream.getAudioTracks()[0]?.setMicrophoneMute(true);
-    // } else {
-    //   mediaStream.getAudioTracks()[0]?.enabled = true;
-    //   mediaStream.getAudioTracks()[0]?.setMicrophoneMute(true);
-    // }
   }
 }
