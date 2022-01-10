4e564e38ac3bcce0c40f0b3c473909b829ded553
Vithani Chandresh <vithanichandresh.g@gmail.com>
2022-01-10 12:26:57 +0530

    complete bid out design

--- lib/ui/screens/user_bid/user_bid_outs_list.dart
+++ lib/ui/screens/user_bid/user_bid_outs_list.dart
@@ -1,4 +1,5 @@
 
+import 'package:app_2i2i/infrastructure/commons/theme.dart';
 import 'package:app_2i2i/infrastructure/models/user_model.dart';
 import 'package:app_2i2i/infrastructure/providers/all_providers.dart';
 import 'package:app_2i2i/ui/screens/home/wait_page.dart';
@@ -6,9 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shimmer/shimmer.dart';
 
-import '../../../infrastructure/data_access_layer/repository/firestore_database.dart';
 import '../../../infrastructure/models/bid_model.dart';
-import 'widgets/no_bid_page.dart';
 
 class UserBidOutsList extends ConsumerWidget {
   UserBidOutsList({
@@ -34,34 +33,103 @@ class UserBidOutsList extends ConsumerWidget {
       return WaitPage();
     }
     List<BidOut> bids =  bidOutList.asData!.value;
-    return ListView.builder(
+    return ListView.separated(
       primary: false,
       physics: NeverScrollableScrollPhysics(),
       itemCount: bids.length,
-      padding: const EdgeInsets.symmetric(horizontal: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 10),
       itemBuilder: (_, ix) {
         BidOut bid = bids[ix];
-        final String num = bid.speed.num.toString();
-        final int assetId = bid.speed.assetId;
-        final String assetIDString = assetId == 0 ? 'ALGO' : assetId.toString();
-        final color = ix % 2 == 0 ? Theme.of(context).primaryColor : Theme.of(context).cardColor;
-
         final bUser = ref.watch(userProvider(bid.B));
 
         if(bUser.asData?.value is UserModel) {
-          return Card(
-            color: color,
-            child: ListTile(
-              trailing: trailingIcon == null
-                  ? null
-                  : IconButton(
-                onPressed: () => onTrailingIconClick!(bid),
-                icon: trailingIcon!,
+          var user = bUser.asData!.value;
+          var statusColor = AppTheme().green;
+          if (user.status == 'OFFLINE') {
+            statusColor = AppTheme().gray;
+          }
+          if (user.locked) {
+            statusColor = AppTheme().red;
+          }
+          String firstNameChar = user.name;
+          if (firstNameChar.length > 0) {
+            firstNameChar = firstNameChar.substring(0, 1);
+          }
+          return ListTile(
+            onTap: () {},
+            leading: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: SizedBox(
+                height: 55,
+                width: 55,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.white, width: 2),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.08),
+                              blurRadius: 20,
+                              spreadRadius: 0.5,
+                            )
+                          ]),
+                      alignment: Alignment.center,
+                      child: Text(
+                        firstNameChar,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        height: 15,
+                        width: 15,
+                        decoration: BoxDecoration(
+                            color: statusColor,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.white, width: 2)),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              title: Text('$num'),
-              subtitle: Text('[$assetIDString/sec]'),
-              // tileColor: color,
-              // onTap: () => onTap(bid),
+            ),
+            title: Text(
+              user.name,
+              maxLines: 1,
+              softWrap: false,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Text(
+              user.bio,
+              maxLines: 2,
+              softWrap: false,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(bid.speed.num.toString() + ' Algo/Sec'),
+                SizedBox(width: 8),
+                GestureDetector(
+                  onTap: (){
+                    onTrailingIconClick?.call(bid);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).iconTheme.color,
+                    ),
+                    padding: EdgeInsets.all(4),
+                    child: Icon(Icons.close,color: Theme.of(context).cardColor,size: 15,),
+                  ),
+                ),
+              ],
             ),
           );
         }
@@ -97,7 +165,12 @@ class UserBidOutsList extends ConsumerWidget {
             ),
           ),
         );
-        
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.only(left: 85),
+          child: Divider(),
+        );
       },
     );
   }
