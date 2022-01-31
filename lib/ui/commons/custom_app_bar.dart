5c70d9f44f1e666aec9eb59efd7785a2327fd140
imi <89938440+2i2i@users.noreply.github.com>
2022-01-31 16:43:34 -0700

    bug

--- lib/ui/commons/custom_app_bar.dart
+++ lib/ui/commons/custom_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:app_2i2i/infrastructure/commons/utils.dart';
-import 'package:app_2i2i/infrastructure/data_access_layer/repository/firestore_database_tests.dart';
 import 'package:app_2i2i/ui/commons/custom_profile_image_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
