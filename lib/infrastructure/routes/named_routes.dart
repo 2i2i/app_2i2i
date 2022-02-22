9a2dbd4985672df19cbf44e953fea81447b2b18f
imi <89938440+2i2i@users.noreply.github.com>
2022-02-22 15:33:15 -0700

    fix bio bug

--- lib/infrastructure/routes/named_routes.dart
+++ lib/infrastructure/routes/named_routes.dart
@@ -321,7 +321,7 @@ class NamedRoutes {
           leading: Container(),
           toolbarHeight: 20,
           title: Text(AlgorandNet.testnet.name +
-              ' - v15' +
+              ' - v16' +
               (updateAvailable ? ' - update: reload page' : '')),
           titleTextStyle: Theme.of(context)
               .textTheme
