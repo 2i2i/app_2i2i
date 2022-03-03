b6d80d3f98103a1fc8cf713bc2bdb115ad2cac3a
Vithani Chandresh <33287130+vithanichandresh@users.noreply.github.com>
2022-03-03 10:57:03 +0530

    Android App Check implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,22 @@
 // createRoom - A
 // import 'package:http/http.dart' as html;
 // import 'dart:html' as html;
+import 'dart:async';
+
+import 'package:app_2i2i/infrastructure/commons/theme.dart';
 import 'package:app_2i2i/infrastructure/models/user_model.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
-import "package:universal_html/html.dart" as html;
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:app_2i2i/infrastructure/commons/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutterfire_ui/i10n.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import "package:universal_html/html.dart" as html;
+
 import 'infrastructure/providers/all_providers.dart';
 import 'infrastructure/routes/named_routes.dart';
 import 'ui/screens/localization/app_localization.dart';
@@ -34,14 +36,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (!kIsWeb) {
     await Firebase.initializeApp();
-    await FirebaseAppCheck.instance.activate();
-  }else{
-    await FirebaseAppCheck.instance.activate(
+  }
+  await FirebaseAppCheck.instance.activate(
       webRecaptchaSiteKey: '6LcASwUeAAAAAE354ZxtASprrBMOGULn4QoqUnze'
     );
-  }
 
-  await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
+  // await FirebaseAppCheck.instance.setTokenAutoRefreshEnabled(true);
 
   //region DEBUG
   // FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
@@ -140,9 +140,8 @@ class _MainWidgetState extends ConsumerState<MainWidget>
         });
       }
 
-      FirebaseAppCheck.instance.onTokenChange.listen((token) {
-        print(token);
-      });
+
+
     });
   }
 
