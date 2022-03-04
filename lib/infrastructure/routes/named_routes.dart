50b0f28bd7214fa6b4189d568a934ae450203099
imi <89938440+2i2i@users.noreply.github.com>
2022-03-04 12:26:23 -0700

    v26

--- lib/infrastructure/routes/named_routes.dart
+++ lib/infrastructure/routes/named_routes.dart
@@ -324,7 +324,7 @@ class NamedRoutes {
           leading: Container(),
           toolbarHeight: 20,
           title: Text(AlgorandNet.testnet.name +
-              ' - v25' +
+              ' - v26' +
               (updateAvailable ? ' - update: reload page' : '')),
           titleTextStyle: Theme.of(context)
               .textTheme
