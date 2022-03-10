c929abb9c4ad4585245bee25efacda6244d2090d
imi <89938440+2i2i@users.noreply.github.com>
2022-03-10 11:39:42 -0700

    catch algoexplorer

--- lib/infrastructure/routes/named_routes.dart
+++ lib/infrastructure/routes/named_routes.dart
@@ -361,7 +361,7 @@ class NamedRoutes {
                 leading: Container(),
                 toolbarHeight: 20,
                 title: Text(AlgorandNet.testnet.name +
-                    ' - v37' +
+                    ' - v38' +
                     (updateAvailable ? ' - update: reload page' : '')),
                 titleTextStyle: Theme.of(context)
                     .textTheme
