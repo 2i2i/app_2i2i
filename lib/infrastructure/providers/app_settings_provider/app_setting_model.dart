66b989fe0e15985542106ea39ea2ebdf6c0456f2
imi <89938440+2i2i@users.noreply.github.com>
2022-07-24 11:25:52 -0600

    v22

--- lib/infrastructure/providers/app_settings_provider/app_setting_model.dart
+++ lib/infrastructure/providers/app_settings_provider/app_setting_model.dart
@@ -66,7 +66,7 @@ class AppSettingModel extends ChangeNotifier {
   }
 
   bool updateRequired = false;
-  String version = "1.0.21";
+  String version = "1.0.22";
 
   Future<void> setThemeMode(String mode) async {
     await storage.write('theme_mode', mode);
