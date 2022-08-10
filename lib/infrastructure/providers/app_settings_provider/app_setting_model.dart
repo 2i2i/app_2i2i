e7c874193b622edee2761b02925c6a3573d18657
1m1-github <77983409+1m1-github@users.noreply.github.com>
2022-08-10 11:02:10 -0600

    v26

--- lib/infrastructure/providers/app_settings_provider/app_setting_model.dart
+++ lib/infrastructure/providers/app_settings_provider/app_setting_model.dart
@@ -83,7 +83,7 @@ class AppSettingModel extends ChangeNotifier {
   }
 
   bool updateRequired = false;
-  String version = "1.0.25";
+  String version = "1.0.26";
 
   Future<void> setThemeMode(String mode) async {
     await storage.write('theme_mode', mode);
