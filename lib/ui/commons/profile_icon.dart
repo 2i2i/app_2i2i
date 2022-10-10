0500fd2402cd8676ab5257e42553c614fdd106f4
vithanichandresh <vithanichandresh.g@gmail.com>
2022-10-10 19:14:01 +0530

    issue: outgoing bid widget ~ click on profile pic should lead to other user. status : Solved.

--- lib/ui/commons/profile_icon.dart
+++ lib/ui/commons/profile_icon.dart
@@ -27,40 +27,41 @@ class ProfileIcon extends ConsumerWidget {
     }
 
     return FutureBuilder(
-        future: SecureStorage().read(Keys.myReadBids),
-        builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-          if (snapshot.data == null) {
-            return normalReturn;
-          }
-          List<String> localIds = snapshot.data!.split(',').toSet().toList();
-          List serverIds = bids.map((e) => e.id).toSet().toList();
-          bool anyNew = serverIds.any((element) => !localIds.contains(element));
-          if (!anyNew) {
-            return normalReturn;
-          }
+      future: SecureStorage().read(Keys.myReadBids),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.data == null) {
+          return normalReturn;
+        }
+        List<String> localIds = snapshot.data!.split(',').toSet().toList();
+        List serverIds = bids.map((e) => e.id).toSet().toList();
+        bool anyNew = serverIds.any((element) => !localIds.contains(element));
+        if (!anyNew) {
+          return normalReturn;
+        }
 
-          return SizedBox(
-            height: 30,
-            width: 30,
-            child: Stack(
-              children: [
-                selectedIcon('assets/icons/person.svg', context),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    height: 15,
-                    width: 15,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.white, width: 2),
-                    ),
+        return SizedBox(
+          height: 30,
+          width: 30,
+          child: Stack(
+            children: [
+              selectedIcon('assets/icons/person.svg', context),
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 15,
+                  width: 15,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 2),
                   ),
                 ),
-              ],
-            ),
-          );
-        });
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Widget selectedIcon(String iconPath, BuildContext context, {bool isSelected = false}) {
