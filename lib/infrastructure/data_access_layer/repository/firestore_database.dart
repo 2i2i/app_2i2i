e7fc2aebb7b72bc2f557829c01a1dd4fec11b10b
vithanichandresh <vithanichandresh.g@gmail.com>
2022-11-16 00:42:57 +0530

    1. Solve Platform issue. 2. Solve Design issue like
    Bottom sheet open in wide of screen,
    login with wallet connect dialog ui, Handle wallet account tile(Subjective/Objective) ui with dark mode and light mode, hide version tile in appsetting and bottom bar.
    and testing

--- lib/infrastructure/data_access_layer/repository/firestore_database.dart
+++ lib/infrastructure/data_access_layer/repository/firestore_database.dart
@@ -13,6 +13,7 @@ import 'package:app_2i2i/infrastructure/models/token_model.dart';
 import 'package:app_2i2i/infrastructure/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
