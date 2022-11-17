0a160d0c50ed942ecbf9bcc2739a4a4ae47cfae4
Vithani Chandresh <vithanichandresh.g@gmail.com>
2022-11-18 01:41:15 +0530

    - set multiple token for user - delete token when user logout

--- lib/infrastructure/models/token_model.dart
+++ lib/infrastructure/models/token_model.dart
@@ -1,21 +1,43 @@
+import 'package:app_2i2i/infrastructure/commons/utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:io';
 
 class TokenModel {
-  final String operatingSystem = Platform.operatingSystem;
-  final String operatingSystemVersion = Platform.operatingSystemVersion;
+  String? operatingSystem;
+  String? operatingSystemVersion;
   String value;
+  Timestamp ts;
 
-  TokenModel({required this.value});
+  TokenModel({required this.value, required this.ts, this.operatingSystem, this.operatingSystemVersion});
 
-  TokenModel.fromJson(Map<String, dynamic> json) : value = json['value'];
+  TokenModel.fromJson(Map<String, dynamic> json)
+      : this.value = '',
+        this.ts = Timestamp.now() {
+    if (json['operatingSystemVersion'] is String) {
+      operatingSystemVersion = json['operatingSystemVersion'];
+    }
+    if (json['operatingSystem'] is String) {
+      operatingSystem = json['operatingSystem'];
+    }
+    if (json['value'] is String) {
+      value = json['value'];
+    }
+    if (json['ts'] is String) {
+      DateTime? date = json['ts'].toString().toDate();
+      if (date != null) {
+        ts = Timestamp.fromDate(date);
+      }
+    }
+    if (json['ts'] is Timestamp) {
+      ts = json['ts'];
+    }
+  }
 
   Map<String, dynamic> toJson() {
     return {
       'operatingSystem': operatingSystem,
       'operatingSystemVersion': operatingSystemVersion,
       'value': value,
-      'ts': FieldValue.serverTimestamp(),
+      'ts': ts,
     };
   }
 }
