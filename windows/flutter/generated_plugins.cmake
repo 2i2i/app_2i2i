43bb346f0a933d9215acfadb824888e2cbc5aeb1
1m1 <email@1m1.io>
2022-11-29 16:53:03 +0400

    banner

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,14 +3,14 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-        connectivity_plus
-        flutter_secure_storage_windows
-        flutter_webrtc
-        permission_handler_windows
-        sentry_flutter
-        share_plus
-        url_launcher_windows
-        )
+  connectivity_plus
+  flutter_secure_storage_windows
+  flutter_webrtc
+  permission_handler_windows
+  sentry_flutter
+  share_plus
+  url_launcher_windows
+)
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
 )
