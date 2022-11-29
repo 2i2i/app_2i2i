c7e7a141867ca59dced4d45455251aa14f4db475
Vithani Chandresh <vithanichandresh.g@gmail.com>
2022-11-29 16:02:53 +0530

    - create and apply extension for date parsing

--- lib/main_test.dart
+++ lib/main_test.dart
@@ -17,7 +17,6 @@ import 'dart:async';
 
 import 'package:app_2i2i/infrastructure/commons/app_config.dart';
 import 'package:app_2i2i/infrastructure/data_access_layer/repository/algorand_service.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
@@ -53,7 +52,7 @@ Future<void> main() async {
               messagingSenderId: "453884442411",
               appId: "1:453884442411:web:dad8591e5125eb8998776e")
           : null);
-  await FirebaseAppCheck.instance.activate(webRecaptchaSiteKey: '6LcASwUeAAAAAE354ZxtASprrBMOGULn4QoqUnze');
+  // await FirebaseAppCheck.instance.activate(webRecaptchaSiteKey: '6LcASwUeAAAAAE354ZxtASprrBMOGULn4QoqUnze');
 
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
